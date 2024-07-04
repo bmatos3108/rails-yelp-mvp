1be8f361360397a5c455a97f92d3b394fc9908cf
Barbara Matos <bmatos3108@gmail.com>
2024-07-04 10:50:16 +0200

    new file:   db/migrate/20240703151147_create_reviews.rb 	modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_03_150019) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_03_151147) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -18,6 +18,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_03_150019) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "phone_number"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "restaurant_id"
+    t.integer "user_id"
+    t.text "content"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
